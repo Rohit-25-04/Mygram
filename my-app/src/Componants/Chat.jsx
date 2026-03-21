fbe18cec07b693e00e40c06fd5f52c0d8c8b4536
Rohit Rawat <rohiirwt08@gmail.com>
2026-03-22 01:29:46 +0530

    Update Chat component for Vercel frontend

--- my-app/src/Componants/Chat.jsx
+++ my-app/src/Componants/Chat.jsx
@@ -36,11 +36,7 @@ import React, { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import { ArrowLeft } from "lucide-react";
 
- const socket = io(import.meta.env.VITE_API_URL, {
-  cors: {
-    origin: "*",
-  },
-});
+ const socket = io(import.meta.env.VITE_API_URL);
 
 function Chat() {
   const [users, setUsers] = useState([]);
