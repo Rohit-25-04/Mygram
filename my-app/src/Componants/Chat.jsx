a0d62427917e449341798374976a865475433361
Rohit Rawat <rohiirwt08@gmail.com>
2026-03-22 10:42:05 +0530

    again chnge coket cors

--- my-app/src/Componants/Chat.jsx
+++ my-app/src/Componants/Chat.jsx
@@ -36,8 +36,8 @@ import React, { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import { ArrowLeft } from "lucide-react";
 
-  const socket = io("https://mygram-production.up.railway.app", {
-  transports: ["websocket", "polling"],
+ const socket = io("https://mygram-production.up.railway.app", {
+  transports: ["polling"], // websocket skip karo abhi
   withCredentials: true
 });
 
